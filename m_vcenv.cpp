47793b1ac18ecdc162d0b21be73e1cba634de34a
mnagorni <mnagorni>
2003-10-27 16:15:39 +0000

    Fixed VC Envelope for decay=0

--- m_vcenv.cpp
+++ m_vcenv.cpp
@@ -20,7 +20,7 @@
 #include "port.h"
 
 M_vcenv::M_vcenv(QWidget* parent, const char *name, SynthData *p_synthdata) 
-              : Module(6, parent, name, p_synthdata) {
+              : Module(1, parent, name, p_synthdata) {
 
   QString qs;
   int l1;
@@ -71,7 +71,7 @@ M_vcenv::M_vcenv(QWidget* parent, const char *name, SynthData *p_synthdata)
     retrigger[l1] = false;
     state[l1] = 0;
     noteActive[l1] = false;
-    e[1] = 0;
+    e[l1] = 0;
   }
   configDialog->addSlider(0, 1, a0, "Attack Offset", &a0);
   configDialog->addSlider(0, 1, d0, "Decay Offset", &d0);
@@ -96,6 +96,24 @@ M_vcenv::M_vcenv(QWidget* parent, const char *name, SynthData *p_synthdata)
 M_vcenv::~M_vcenv() {
 }
 
+void M_vcenv::paintEvent(QPaintEvent *ev) {
+  
+  QPainter p(this);
+  QString qs;
+  int l1;
+
+  for (l1 = 0; l1 < 4; l1++) {
+    p.setPen(QColor(195 + 20*l1, 195 + 20*l1, 195 + 20*l1));
+    p.drawRect(l1, l1, width()-2*l1, height()-2*l1);
+  }
+  p.setPen(QColor(255, 255, 255));
+  p.setFont(QFont("Helvetica", 10));
+  p.drawText(10, 20, "VC Envelope");
+  p.setFont(QFont("Helvetica", 8)); 
+  qs.sprintf("ID %d", moduleID);
+  p.drawText(15, 32, qs);
+}
+
 void M_vcenv::generateCycle() {
 
   int l1, l2;
@@ -103,14 +121,81 @@ void M_vcenv::generateCycle() {
 
   if (!cycleReady) {
     cycleProcessing = true;
-
-    gateData = port_M_gate->getinputdata ();
-    retriggerData = port_M_retrigger->getinputdata ();
-    attackData = port_M_attack->getinputdata ();
-    decayData = port_M_decay->getinputdata ();
-    sustainData = port_M_sustain->getinputdata ();
-    releaseData = port_M_release->getinputdata ();
-
+    for (l1 = 0; l1 < synthdata->poly; l1++) {
+      memcpy(lastdata[0][l1], data[0][l1], synthdata->cyclesize * sizeof(float));
+    }
+    if (port_M_gate->connectedPortList.count()) {
+      in_M_gate = (Module *)port_M_gate->connectedPortList.at(0)->parentModule;
+      if (!in_M_gate->cycleProcessing) {
+        in_M_gate->generateCycle();
+        gateData = in_M_gate->data[port_M_gate->connectedPortList.at(0)->index];
+      } else {
+        gateData = in_M_gate->lastdata[port_M_gate->connectedPortList.at(0)->index];
+      }
+    } else {
+      in_M_gate = NULL;
+      gateData = synthdata->zeroModuleData;
+    }
+    if (port_M_retrigger->connectedPortList.count()) {
+      in_M_retrigger = (Module *)port_M_retrigger->connectedPortList.at(0)->parentModule;
+      if (!in_M_retrigger->cycleProcessing) {
+        in_M_retrigger->generateCycle();
+        retriggerData = in_M_retrigger->data[port_M_retrigger->connectedPortList.at(0)->index];
+      } else {
+        retriggerData = in_M_retrigger->lastdata[port_M_retrigger->connectedPortList.at(0)->index];
+      }
+    } else {
+      in_M_retrigger = NULL;
+      retriggerData = synthdata->zeroModuleData;
+    }
+    if (port_M_attack->connectedPortList.count()) {
+      in_M_attack = (Module *)port_M_attack->connectedPortList.at(0)->parentModule;
+      if (!in_M_attack->cycleProcessing) {
+        in_M_attack->generateCycle();
+        attackData = in_M_attack->data[port_M_attack->connectedPortList.at(0)->index];
+      } else {
+        attackData = in_M_attack->lastdata[port_M_attack->connectedPortList.at(0)->index];
+      }
+    } else {
+      in_M_attack = NULL;
+      attackData = synthdata->zeroModuleData;
+    }
+    if (port_M_decay->connectedPortList.count()) {
+      in_M_decay = (Module *)port_M_decay->connectedPortList.at(0)->parentModule;
+      if (!in_M_decay->cycleProcessing) {
+        in_M_decay->generateCycle();
+        decayData = in_M_decay->data[port_M_decay->connectedPortList.at(0)->index];
+      } else {
+        decayData = in_M_decay->lastdata[port_M_decay->connectedPortList.at(0)->index];
+      }
+    } else {
+      in_M_decay = NULL;
+      decayData = synthdata->zeroModuleData;
+    }
+    if (port_M_sustain->connectedPortList.count()) {
+      in_M_sustain = (Module *)port_M_sustain->connectedPortList.at(0)->parentModule;
+      if (!in_M_sustain->cycleProcessing) {
+        in_M_sustain->generateCycle();
+        sustainData = in_M_sustain->data[port_M_sustain->connectedPortList.at(0)->index];
+      } else {
+        sustainData = in_M_sustain->lastdata[port_M_sustain->connectedPortList.at(0)->index];
+      }
+    } else {
+      in_M_sustain = NULL;
+      sustainData = synthdata->zeroModuleData;
+    }
+    if (port_M_release->connectedPortList.count()) {
+      in_M_release = (Module *)port_M_release->connectedPortList.at(0)->parentModule;
+      if (!in_M_release->cycleProcessing) {
+        in_M_release->generateCycle();
+        releaseData = in_M_release->data[port_M_release->connectedPortList.at(0)->index];
+      } else {
+        releaseData = in_M_release->lastdata[port_M_release->connectedPortList.at(0)->index];
+      }
+    } else {
+      in_M_release = NULL;
+      releaseData = synthdata->zeroModuleData;
+    }
     switch(timeScale) {
       case 0: ts = 0.1;
               break;
@@ -123,7 +208,7 @@ void M_vcenv::generateCycle() {
     tsr = ts / (double)synthdata->rate;
     tsn = ts * (double)synthdata->rate;
     for (l1 = 0; l1 < synthdata->poly; l1++) {
-//      fprintf(stderr, "gate:%d retrigger:%d noteActive:%d state: %d\n", gate[l1], retrigger[l1], noteActive[l1], state[l1]);
+//      fprintf(stderr, "l1: %d gate:%d retrigger:%d noteActive:%d state: %d e: %f\n", l1, gate[l1], retrigger[l1], noteActive[l1], state[l1], e[l1]);
       for (l2 = 0; l2 < synthdata->cyclesize; l2++) {
         if (!gate[l1] && gateData[l1][l2] > 0.5) {
           gate[l1] = true;
@@ -153,7 +238,7 @@ void M_vcenv::generateCycle() {
                   break;
           case 2: if (decayReleaseMode) {
                     n = tsn * (d0 + dGain * decayData[l1][l2]);
-                    if (n < 0) n = 0;
+                    if (n <= 1) n = 1;
                     c = 2.3 / n; 
                     e[l1] *= exp(-c);            
                   } else {
@@ -161,8 +246,9 @@ void M_vcenv::generateCycle() {
                   }
                   if (e[l1] <= s0 + sGain * sustainData[l1][l2]) {
                     state[l1] = 3;
+                  } else {
+                    break;
                   }
-                  break;
           case 3: e[l1] = s0 + sGain * sustainData[l1][l2];
                   break;
           case 4: if (decayReleaseMode) {
