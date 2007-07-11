8063ddbbb527271227cb7ff85d4996d37c12c302
fzu <fzu>
2007-07-11 12:11:23 +0000

    realtime-ish midi

--- midislider.h
+++ midislider.h
@@ -4,37 +4,16 @@
 #include "midisliderbase.h"
 
 /**
-  *@author Matthias Nagorni
+  *@author Karsten Wiese
   */
 
 class MidiSlider : public MidiSliderBase {
-Q_OBJECT
-
 public:
-  bool isLog, isMaster;
-  float *valueRef;
-  float min, max, initial_min, initial_max;
-  
-public:
-  MidiSlider(Module *parentModule, float minValue, float maxValue, float pageStep, float value, Qt::Orientation orientation,
-             QWidget * parent, const QString &name, float *valueRef=0, bool p_isLog=false);
+  MidiSlider(class MidiControllableFloat &mcAble, Qt::Orientation orientation = Qt::Horizontal);
+  virtual MidiGUIcomponent *createTwin();
 
-  virtual void setMidiValue(int value);
-  virtual int getMidiValue();
   float getValue();
-  
-signals:
-  void valueChanged(int value);
-  void logModeChanged(bool on);
-	
-public slots:
-  void updateValue(int);
-  void setLogMode(bool on);
-  void setNewMin();
-  void setNewMax();
-  void setNewMin(int p_min);
-  void setNewMax(int p_max);
-  void setInitialMinMax();
+  void minMaxChanged();
 };
 
 #endif
